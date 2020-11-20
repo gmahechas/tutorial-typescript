5ec83f6a4e18288492536396d6ef4f4a24c3e56d
gmahechas <gmahechas@ingenieros.com>
2020-11-19 20:13:21 -0500

    you neeed to do from buuble order, 3 app

--- 04-sort/src/index.ts
+++ 04-sort/src/index.ts
@@ -1,8 +1,25 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
+import { CharacteresCollection } from './CharactersCollection';
+import { LinkedList } from './LinkedList';
 
-const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
+/* const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
+const sorterNumbers = new Sorter(numbersCollection);
+sorterNumbers.sort();
+console.log(numbersCollection.data); */
 
-const sorter = new Sorter(numbersCollection);
+
+/* const charactersCollection = new CharacteresCollection('lahoatodos')
+const sorterCharacters = new Sorter(charactersCollection);
+sorterCharacters.sort();
+console.log(charactersCollection.data); */
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+
+const sorter = new Sorter(linkedList);
 sorter.sort();
-console.log(numbersCollection.data);
+linkedList.print();
